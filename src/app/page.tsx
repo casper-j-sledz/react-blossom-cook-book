4aeaf813615e8ec80106f72b8490555cf716d67e
Casper J. Sledz <casper.j.sledz@gmail.com>
2024-07-05 09:35:34 +0200

    RouterProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,39 +4,52 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 import { DemoAnchor } from "./demos";
 import { RecipeGrid } from "./recipe-grid.component";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-// TODO: 
-// ustawienia, język polski, angielski
-// łatwe wyszukiwanie przepisów
-// responsywna splikacja
-// użycie darmowych ikon i obrazów
-// tworzenie szybkich list zakupów
-// porcje mała duża średnia zdefiniuj własną
+function HomePage() {
+  return <h1>Home Page</h1>
+}
+
+function ProductsPage() {
+  return <h1>Products Page</h1>
+}
+
+function Logo() {
+  return (
+    <div className={styles.description}>
+    <div>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        By{" "}
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          className={styles.vercelLogo}
+          width={100}
+          height={24}
+          priority
+        />
+      </a>
+    </div>
+  </div>
+  )
+}
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage/>},
+  { path: '/products', element: <ProductsPage/>},
+  { path: '/recipes', element: <RecipeGrid/>},
+  { path: '/demo', element: <DemoAnchor/>},
+]);
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-      <RecipeGrid/>
-      {/* <DemoAnchor/> */}
+      <Logo/>
+      <RouterProvider router={router}/>
     </main>
   );
 }
